e29ad97ac8802e9ce7024e15a2a194349330989d
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2024-12-18 11:13:45 -0500

    solve question 3 js exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let splitWord = word.split("");
+let reverseArray = splitWord.reverse();
+let joinArray = reverseArray.join("");
 
+return joinArray;
 };
+
+reverseString("hello");
+
+reverseString("hello there");
+
+reverseString('123! abc! Hello, Odinite.');
+
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
