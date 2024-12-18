a7df028284a033f839f8a5a8523b8fbceb6215a1
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2024-12-18 16:37:02 -0500

    solve question 4 js-exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,37 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+const newArray=[];
+
+array.forEach((item) => {
+    if(!args.includes(item)) {
+    newArray.push(item);
+    }
+    })
+
+    return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2);
+
+removeFromArray([1, 2, 2, 3], 2);
+
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+
+removeFromArray([1, 2, 3, 4], 7, 2);
+
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+
+removeFromArray([1, 2, 3], "1", 3);
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// array, args
+// take args out of array
