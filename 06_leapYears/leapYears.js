7bb42743ddc32f08d9261a9474152d4440fbbbef
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2024-12-19 10:19:18 -0500

    solve question 6 js-exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+
+if ((year%4 == 0) && ((year%100 !=0) || (year%400 == 0))) {
+    return true;
+} else {
+    return false;
+}
 
 };
+
+leapYears(1996);
+
+leapYears(1997);
+
+leapYears(34992);
+
+leapYears(1900);
+
+leapYears(1600);
+
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
