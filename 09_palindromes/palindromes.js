7e388c967c1fab16925a7c87323aab0c7e529a55
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-01-13 15:37:51 -0500

    solve js-exercises question 8 and 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+const lettersNum = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+// remove punctuation and make lower case and make the first string
+const cleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((item) => lettersNum.includes(item))
+    .join("");
+
+// split into array, reverse array, join array to make second string
+const reverseString = cleanString
+    .split("")
+    .reverse()
+    .join("");
+
+// compare both strings
+
+if (cleanString == reverseString) { 
+return true;    
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
