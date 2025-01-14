c02a60aacf7a207004e0c0f3f1fcc3ae10bd864a
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-01-14 09:32:01 -0500

    solve question 10 js-exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (typeof number === 'string') {
+        fibNumber = parseInt(number);
+    } else {
+        fibNumber = number;
+    }
+    
+    if (fibNumber < 0) {
+        return "OOPS";
+
+    } else if (fibNumber == 0) {
+        return 0;
+
+    } else if (fibNumber > 0) {
+
+        let numOne = 0;
+        let numTwo = 1;
+
+        for (i = 2; i <= number; i++) {
+            let current = numOne + numTwo;
+
+            // next time it runs on these two numbers
+            numOne = numTwo;
+            numTwo = current;
+        };
+
+        return numTwo;
+
+    }
+
+    
 
 };
 
