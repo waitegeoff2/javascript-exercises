b50721f285d1addcf5d4e698ba88aafc35ef3d4b
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-01-14 12:17:23 -0500

    solve question 12 js-exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    } 
+
+    return death - birth;
 
 };
+
+
+const findTheOldest = function(people) {
+       
+
+    const orderedAge = people.sort((a,b) => 
+        getAge(b.yearOfBirth, b.yearOfDeath) - getAge(a.yearOfBirth, a.yearOfDeath));
+    
+    const oldestPerson = orderedAge[0];
+    return oldestPerson;
+
+    };
+
+    // const findTheOldest = function (people) {
+    //     const peopleOldestToYoungest = people.toSorted(
+    //       (person, nextPerson) => getAge(nextPerson) - getAge(person),
+    //     );
+      
+    //     const oldestPerson = peopleOldestToYoungest[0];
+    //     return oldestPerson;
+    //   };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
